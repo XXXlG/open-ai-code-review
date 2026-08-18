b249314c4eadb121d27ca8ef2594369f8e602426
xxliang <18561144539@163.com>
2026-08-18 15:15:50 +0800

    # test: test@v3

--- open-ai-code-review-sdk/src/main/java/com/xxliang/middleware/sdk/infrastructure/feishu/FeishuNotifier.java
+++ open-ai-code-review-sdk/src/main/java/com/xxliang/middleware/sdk/infrastructure/feishu/FeishuNotifier.java
@@ -75,6 +75,8 @@ public class FeishuNotifier {
     public boolean sendRichTextMessage(String title, String content, String logUrl) {
         try {
             String postContent = buildPostContent(title, content, logUrl);
+            logger.info("DEBUG:----Feishu发送的富文本消息-----\n");
+            logger.info(postContent);
             return sendMessage("post", postContent);
         } catch (Exception e) {
             logger.error("发送飞书富文本消息失败", e);
